d5e88170684996a4c3e9338b56d4387fa2d76780
Wende Lopez <wendelopez@gmail.com>
2022-05-08 14:00:58 +0200

    Clase Turno

--- src/turno/Turno.java
+++ src/turno/Turno.java
@@ -2,4 +2,36 @@ package turno;
 
 public class Turno {
     
+    private int numTurno;
+    private String accionEntrenador;
+    private String accionRival;
+
+    public Turno(){
+    this.numTurno = 0;
+    this.accionEntrenador= "";
+    this.accionRival= "";
+    }
+    public int getNumTurno() {
+        return numTurno;
+    }
+
+    public void setNumTurno(int numTurno) {
+        this.numTurno = numTurno;
+    }
+    
+    public String getAccionEntrenador() {
+        return accionEntrenador;
+    }
+
+    public void setAccionEntrenador(String accionEntrenador) {
+        this.accionEntrenador = accionEntrenador;
+    }
+    
+    public String getAccionRival() {
+        return accionRival;
+    }
+
+    public void setAccionRival(String accionRival) {
+        this.accionRival = accionRival;
+    }
 }
